e342485c8063eb89f8eb36866bf25ceb93397fdb
Amila-Software-Engineer <amilasoften@gmail.com>
2022-11-08 09:38:09 +0530

    model user created

--- src/com/seekescloud/pos/controllers/LoginFormController.java
+++ src/com/seekescloud/pos/controllers/LoginFormController.java
@@ -1,4 +1,22 @@
 package com.seekescloud.pos.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public AnchorPane loginFormContext;
+
+    public void createAnAccountOnAction(ActionEvent actionEvent) {
+
+    }
+
+    private void setUi(String location) throws IOException {
+        Stage window = (Stage)loginFormContext.getScene().getWindow();
+        window.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/"+location+".fxml"))));
+    }
 }
---
+++ src/com/seekescloud/pos/db/Database.java
@@ -0,0 +1,10 @@
+package com.seekescloud.pos.db;
+
+import com.seekescloud.pos.model.User;
+
+import java.util.ArrayList;
+
+public class Database {
+
+    public  static ArrayList<User> usertable = new ArrayList<User>();
+}
